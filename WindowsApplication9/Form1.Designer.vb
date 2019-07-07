f3b099d6a87883e433ed99a69e2d9451610728b0
jinxiaoqiang <147451990@qq.com>
2019-07-07 23:09:06 +0800

    修改 hello.php 文件

--- WindowsApplication9/Form1.Designer.vb
+++ WindowsApplication9/Form1.Designer.vb
@@ -22,9 +22,54 @@ Partial Class Form1
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(47, 66)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(183, 66)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(122, 171)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
 End Class
--- WindowsApplication9/Form1.vb
+++ WindowsApplication9/Form1.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Form1
+    Dim sqlConnection1 As SqlClient.SqlConnection
+    Dim strConnet As String = "data source=192.168.43.15;initial catalog =atguigu;user id=sa;password=11099wo;"
+    Dim strConnet1 As String = "Data Source=192.168.41.15;Initial Catalog=atguigu;Integrated Security=true;"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        sqlConnection1 = New System.Data.SqlClient.SqlConnection(strConnet)
 
+        sqlConnection1.Open()
+        MsgBox(sqlConnection1.ToString())
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        sqlConnection1.Close()
+
+    End Sub
 End Class
